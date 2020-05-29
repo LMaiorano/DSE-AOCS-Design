18629989a261ee414384f06ef7d15c4d8934b202
Luigi Maiorano <lmaiorano97@gmail.com>
2020-05-29 12:11:35 +0200

    found workaround to module import error

--- sizing_model.py
+++ sizing_model.py
@@ -8,11 +8,21 @@ date: 5/28/2020
 author: lmaio
 """
 
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.abspath(os.curdir).split('DSE-Mars-Reveal', 1)[0], 'DSE-Mars-Reveal'))
+
+
 from definitions import MarsReveal
 from LuigiPyTools import LatexPandas as LP
 import numpy as np
 import pandas as pd
 
+M = MarsReveal()
+
+
+print(f'mu mars: {M.mu_mars}')
 
 
 
