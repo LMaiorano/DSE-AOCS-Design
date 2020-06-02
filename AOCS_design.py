b18495f1db00f8b1be8dbb934ebc9144db8d30ee
Luigi Maiorano <lmaiorano97@gmail.com>
2020-06-02 09:25:33 +0200

    excel to dict

--- AOCS_design.py
+++ AOCS_design.py
@@ -19,36 +19,38 @@ from LuigiPyTools import LatexPandas as LP
 import numpy as np
 import pandas as pd
 
-M = MarsReveal()
 
 
-def excel_io_dict(filename, **kwargs):
-    '''Loads parameters from excel file to dictionary
 
-    Args:
-        filename: str
-            Path to Excel file
-        **kwargs: 'sheet_name' or 'columns'
-            To specify specific sheet or columns to load
-            - 'sheet_name'='AOCS' (default)
-            - 'columns' = '['Output name', 'Output Value', 'Units']' (default)
-
-    Returns:
-        Dictionary of parameters
-    '''
-    sheet = kwargs.pop('sheet_name', 'AOCS')
-    cols = kwargs.pop('columns', ['Output name', 'Output Value', 'Units'])
-
-    df = pd.read_excel(filename, sheet_name=sheet, usecols=cols)
-    df.set_index('Output name', inplace=True)
-    df.rename(columns={'Output name': 'name', 'Output Value': 'value', 'Units' : 'units'}, inplace=True)
-    return df.to_dict('index')
+# def excel_io_dict(filename, **kwargs):
+#     '''Loads parameters from excel file to dictionary
+#
+#     Args:
+#         filename: str
+#             Path to Excel file
+#         **kwargs: 'sheet_name' or 'columns'
+#             To specify specific sheet or columns to load
+#             - 'sheet_name'='AOCS' (default)
+#             - 'columns' = '['Output name', 'Output Value', 'Units']' (default)
+#
+#     Returns:
+#         Dictionary of parameters
+#     '''
+#     sheet = kwargs.pop('sheet_name', 'AOCS')
+#     cols = kwargs.pop('columns', ['Output name', 'Output Value', 'Units'])
+#
+#     df = pd.read_excel(filename, sheet_name=sheet, usecols=cols)
+#     df.set_index('Output name', inplace=True)
+#     df.rename(columns={'Output name': 'name', 'Output Value': 'value', 'Units' : 'units'}, inplace=True)
+#     return df.to_dict('index')
 
 
 
 
 if __name__ == '__main__':
+    M = MarsReveal()
+
     file = 'Sub_Output.xlsx'
-    params = excel_io_dict(file)
+    params = M.excel_io_dict(file)
     print(params['test 2']['value'])
 
